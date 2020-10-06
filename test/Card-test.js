83afbd556fd3ee8e6f470da40c328f32af7a1603
Kara Caputo <kncaputo@gmail.com>
2020-10-05 20:49:56 -0400

    Refactor expect statements

--- test/Card-test.js
+++ test/Card-test.js
@@ -22,7 +22,7 @@ describe('Card', function() {
 
   it('id should be a number', function() {
     const card = new Card(1);
-    expect(typeof card.id).to.equal('number')
+    expect(card.id).to.be.a('number')
   })
 
   it('should store a question', function() {
@@ -37,7 +37,7 @@ describe('Card', function() {
 
   it('possible answers should be stored in an array', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(Array.isArray(card.answers)).to.deep.equal(true);
+    expect(card.answers).to.be.an('array');
   });
 
   it('should store the correct answer', function() {
@@ -47,6 +47,6 @@ describe('Card', function() {
 
   it('correct answer should be a string', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    expect(typeof card.correctAnswer).to.equal('string');
+    expect(card.correctAnswer).to.be.a('string');
   })
 });
